c2ee28413703dcbbfe260bd5e14f7c7a19ee78a5
Abdullah Awan <ayzad01@gmail.com>
2022-02-26 19:13:02 -0500

    Section 2, Lesson 7 Starting from our domain

--- services/users_service.go
+++ services/users_service.go
@@ -1,1 +1,10 @@
 package services
+
+import (
+	"bookstore_user-api/domain/users"
+	"bookstore_user-api/utils/errors"
+)
+
+func CreateUser(user users.User) (*users.User, *errors.RestErr) {
+	return &user, nil
+}
